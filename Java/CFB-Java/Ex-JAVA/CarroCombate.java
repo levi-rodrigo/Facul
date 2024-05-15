a4524935276eb83e0f7536c3e2a38c13cc4064c8
Levi Rodrigo <levirodrigolr@gmail.com>
2024-05-15 20:16:19 -0300

    att

--- Java/CFB-Java/Ex-JAVA/CarroCombate.java
+++ Java/CFB-Java/Ex-JAVA/CarroCombate.java
@@ -1,16 +1,37 @@
-public class CarroCombate {
+public class CarroCombate extends Carro {
     private final int MAX_ARMAMENTO=100;
     private final int MIN_ARMAMENTO=0;
     private int qtdeArmamento;
 
-    public CarroCombate(String nome, int blindagem){
+    public CarroCombate(String nome,int blindagem){
         super(nome);
         setArmamento(true);
         setBlindagem(blindagem);
         this.qtdeArmamento=100;
     }
 
-    public void setQtdeArmamento(int armamento){
+    public void setQtdeArmamento(int qtdeArmamento){
+        this.qtdeArmamento+=qtdeArmamento;
+        if(this.qtdeArmamento > MAX_ARMAMENTO){
+            this.qtdeArmamento=MAX_ARMAMENTO;
+        }
+        if(this.qtdeArmamento < MIN_ARMAMENTO){
+            this.qtdeArmamento=MIN_ARMAMENTO;
+        }
+    }
+    public int getQtdeArmamento(){
+        return this.qtdeArmamento;
+    }
+    public void atirar(){
+        if(this.qtdeArmamento > MIN_ARMAMENTO){
+            setQtdeArmamento(-1);
+        }else{
+            System.out.println("Sem Municao");
+        }
+    }
+    public void info(){
+        super.info();
+        System.out.printf("Qtde.Arm.......:%s%n",this.qtdeArmamento);
 
     }
 }
--- Java/CFB-Java/Ex-JAVA/heranca.java
+++ Java/CFB-Java/Ex-JAVA/heranca.java
@@ -2,10 +2,21 @@ public class heranca {
     public static void main(String[] args){
 
     Carro c1=new Carro("Golf");
+    Carro c2=new Carro("HRV");
+    CarroCombate c3=new CarroCombate("Leopardo", 100);
+    CarroCombate c4=new CarroCombate("Pantera", 50);
 
-    c1.setLigado(true);
-    c1.info();
+        c3.atirar();
+        c3.atirar();
+        c3.atirar();
 
+        c4.sofrerDano(30);
 
+        c1.sofrerDano(30);
+
+        c1.info();
+        c2.info();
+        c3.info();
+        c4.info();
     }
 }
